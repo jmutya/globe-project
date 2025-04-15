a3239ecaeac7e4dd44c7a26c83ae98280fce451a
jmutya <j.mutya.477708@umindanao.edu.ph>
2025-04-15 16:03:12 +0800

    haha

--- src/components/Sidebar/sidebar.jsx
+++ src/components/Sidebar/sidebar.jsx
@@ -102,19 +102,23 @@ const Sidebar = ({ user }) => {
 
             {/* Main Content */}
             <div className="flex-1 p-4">
-                <div className="flex justify-between items-center mb-6">
-                    <SearchBar />
-                    <div className="flex items-center space-x-4">
-                        <div className="w-4 h-4 rounded-full bg-green-500" />
-                        <div className="flex items-center space-x-2">
-                            <span>
-                                <p className="text-sm font-semibold">
-                                    {user?.email?.split("@")[0]}
-                                </p>
-                            </span>
-                        </div>
-                    </div>
-                </div>
+               <div className="flex justify-between items-center mb-6">
+    <h1 className="text-3xl font-semibold text-gray-900 tracking-tight capitalize font-poppins">
+        {selected}
+    </h1>
+
+    <div className="flex items-center space-x-4">
+        <div className="w-4 h-4 rounded-full bg-green-500" />
+        <div className="flex items-center space-x-2">
+            <span>
+                <p className="text-sm font-medium text-gray-700 font-poppins">
+                    {user?.email?.split("@")[0]}
+                </p>
+            </span>
+        </div>
+    </div>
+</div>
+
                 <MainContent renderContent={renderContent} />
             </div>
 
