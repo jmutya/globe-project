239fa41dedeaffc87eb4a0837c9cd8838c3b7ae4
jmutya <j.mutya.477708@umindanao.edu.ph>
2025-05-27 10:25:07 +0800

    commit/ improving the UI

--- src/components/content/insight/InsightContent/ClosingAccuracy.jsx
+++ src/components/content/insight/InsightContent/ClosingAccuracy.jsx
@@ -340,7 +340,7 @@ function ClosingAccuracy() {
                   )}
                 </div>
               </th>
-              <th className="border px-4 py-2 text-left">Accuracy (%)</th>
+              <th className="border px-4 py-2 text-left">Accuracy Percentage</th>
             </tr>
           </thead>
           <tbody>
